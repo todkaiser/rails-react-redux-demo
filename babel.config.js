6cb9d0c5defcb7abb411197c7250026d20bbb387
Tod Kaiser <todkaiser@gmail.com>
2022-06-26 07:53:10 -0700

    Remove verbose bable warning messages in webpack

--- babel.config.js
+++ babel.config.js
@@ -51,6 +51,12 @@ module.exports = function(api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
         '@babel/plugin-proposal-class-properties',
         {
           loose: true

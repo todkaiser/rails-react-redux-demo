eb62a39392bd84eb54a3130319b369e51671e430
Tod Kaiser <todkaiser@gmail.com>
2022-06-26 08:01:37 -0700

    Nit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-if [ -f /app/tmp/pids/server.pid ]; then
-  rm /app/tmp/pids/server.pid
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
 fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
